a14665ee7329cc1799f4a66dfac3906df23f534d
Josh Perez <josh@goatslacker.com>
2013-03-13 10:34:57 -0700

    Allows additional dependencies to be injected

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ DI.prototype.register = function (name, fn) {
   this._registers[name] = fn
 }
 
-DI.prototype.inject = function (fn) {
+DI.prototype.getParameterNames = getParameterNames
+
+DI.prototype.inject = function (fn, additionalDependencies) {
   var params = getParameterNames(fn)
+
+  if (additionalDependencies) {
+    Object.keys(additionalDependencies).forEach(function (dependency) {
+      this.register(dependency, additionalDependencies[dependency])
+    }.bind(this))
+  }
+
   var dependencies = params.map(function (param) {
     if (this._registers.hasOwnProperty(param)) {
       return this._registers[param]
